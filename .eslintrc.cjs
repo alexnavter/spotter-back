e1e6439602154efdff6fd0861cafe91f6b9d96d1
Alex Navarro <alexnavter@gmail.com>
2023-03-06 18:50:34 +0100

    Create Github audit & testing workflows

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,19 +6,29 @@ module.exports = {
   extends: ["xo", "prettier"],
   overrides: [
     {
-      extends: ["xo-typescript", "prettier"],
-      files: ["*.ts"],
       rules: {
         "@typescript-eslint/consistent-type-definitions": [
           "error",
           "interface",
         ],
+
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/consistent-type-assertions": "off",
       },
+      extends: ["xo-typescript", "prettier"],
+      files: "*.ts",
+    },
+    {
+      files: ["src/**/models/**/*.ts"],
+      rules: { "@typescript-eslint/naming-convention": "off" },
     },
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "new-cap": ["error", { capIsNewExceptions: ["Router"] }],
+    "no-implicit-coercion": "off",
+  },
 };
