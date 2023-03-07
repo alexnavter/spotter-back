c06a37b5755db1ad16b9dbc9ec61646e138e94ef
Alex Navarro <alexnavter@gmail.com>
2023-03-07 19:01:38 +0100

    Create loginUserSchema & add validation to loginUser usersRouter

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import { validate } from "express-validation";
 import { loginUser } from "../server/controllers/userControllers.js";
+import loginUserSchema from "../server/schemas/loginUserSchema.js";
 
 export const usersRouter = Router();
 
-usersRouter.post("/login", loginUser);
+usersRouter.post(
+  "/login",
+  validate(loginUserSchema, {}, { abortEarly: false }),
+  loginUser
+);
