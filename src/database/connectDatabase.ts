9327a9583575e729b4a696facade5ae76238394b
Alex Navarro <alexnavter@gmail.com>
2023-03-07 13:27:07 +0100

    Create connectDatabase function, relocate debug

---
+++ src/database/connectDatabase.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async (url: string) => {
+  mongoose.set("strictQuery", false);
+  mongoose.set("debug", true);
+  mongoose.set("toJSON", {
+    virtuals: true,
+    transform(doc, ret) {
+      delete ret._id;
+      delete ret.__v;
+    },
+  });
+  await mongoose.connect(url);
+};
+
+export default connectDatabase;
--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -4,7 +4,5 @@ import { app } from "./index.js";
 const debug = createDebug("spotter-api:startServer");
 
 export const startServer = (port: number) => {
-  debug(`Server started at http://localhost:${port}`);
-
   app.listen(port);
 };
